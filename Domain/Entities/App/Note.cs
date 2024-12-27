97f3d72a8e5fa031738bf718c563622b29bcbe1e
Mihneas22 <mihnea.serban06@gmail.com>
2024-12-27 10:04:43 +0200

    entities

--- Domain/Entities/App/Note.cs
+++ Domain/Entities/App/Note.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,21 @@ namespace Domain.Entities.App
 {
     public class Note
     {
+        [Key]
+        public int Id { get; set; }
 
+        public string? Name { get; set; }
+
+        public string? Text { get; set; }
+
+        public int? Progress { get; set; }
+
+        public List<string?> Tags { get; set; }
+
+        public List<string?> SharedKeys { get; set; }
+
+        public DateTime FinalTerm { get; set; }
+
+        public DateTime AddedDateTime { get; set; }
     }
 }
---
+++ Domain/Entities/Keys/ApiKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Keys
+{
+    public class ApiKey
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Key { get; set; }
+
+        public DateTime? AddedTime {  get; set; }
+    }
+}
